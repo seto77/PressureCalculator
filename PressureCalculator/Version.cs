0c7f3ee45ff2698468e990fde4e7ceb79f2f25ec
seto <seto77@gmail.com>
2021-09-01 18:42:45 +0900

    update

--- PressureCalculator/Version.cs
+++ PressureCalculator/Version.cs
@@ -16,7 +16,7 @@ namespace PressureCalculator
 
         static public string History =
            "History" +
-           "\r\n ver1.003(2021/09/01) Changed a framework to .Net 5.0; Added two diamond Raman equations (Fratanduono et al., 2021)." +
+           "\r\n ver1.003(2021/09/01) Fixed a minor bug in Celsius degree mode. The lower limit of temperature was set." +
            "\r\n ver1.002(2021/07/03) Changed a framework to .Net 5.0; Added two diamond Raman equations (Fratanduono et al., 2021)." +
            "\r\n ver1.001(2021/05/15) Distribution site is changed to GitHub." +
            "\r\n ver0.000(???/??/??)  "
